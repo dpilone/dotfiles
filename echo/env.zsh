bc44f03989081e1cbca5838afc51c4c76d6b4c5e
Dan Pilone <dan@element84.com>
2013-03-22 20:23:44 -0400

    Add ECHO workspace environment variable.

---
+++ echo/env.zsh
@@ -0,0 +1,5 @@
+########################################
+# Workspace definition for echo-deployments
+########################################
+
+export WORKSPACE_HOME='/Users/dpilone/Documents/NASA/code/echo_10_latest'
