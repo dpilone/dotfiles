c5d0a0d71f19dd12b5590b616d6d85d181fac910
Dan Pilone <dan@element84.com>
2013-04-24 00:25:45 -0400

    Minor configuration option updates.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -17,20 +17,22 @@ HISTSIZE=10000
 SAVEHIST=10000
 
 setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
+setopt NO_HUP # don't hang upon backgrounded jobs
+setopt NO_LIST_BEEP # don't beep during tab completion
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
+setopt HIST_VERIFY # don't execute a ! history expression, just show it.
+setopt SHARE_HISTORY # share history between sessions 
 setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
+setopt PROMPT_SUBST # parameter, command, and math substitions are done inline
+setopt CORRECT # try to correct the spelling of commands
+setopt COMPLETE_IN_WORD # completion can be done from both ends.
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
+setopt HIST_REDUCE_BLANKS # don't store lines that start with spaces
+setopt HIST_NO_STORE # don't put the history command in history
+setopt NOTIFY # report the status of background jobs immediately
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
