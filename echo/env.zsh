6245d547377eec05142246c3a5a578b6e66bda4a
Dan Pilone <dan@element84.com>
2014-01-22 09:37:40 -0500

    Updates the project path.

--- echo/env.zsh
+++ echo/env.zsh
@@ -2,5 +2,5 @@
 # Workspace definition for echo-deployments
 ########################################
 
-export WORKSPACE_HOME='/Users/dpilone/Documents/NASA/code/echo_10_latest'
+export WORKSPACE_HOME='/Users/dpilone/Projects/NASA/echo_10_latest'
 export ANT_OPTS='-Xmx1024m -Xms512m -XX:MaxPermSize=256m'
