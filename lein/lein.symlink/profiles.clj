bd490be26abd82449bafb40994e007bf07e26180
Dan Pilone <dan@element84.com>
2015-09-03 22:37:16 -0400

    Adds Eastwood for Clojure linting.

--- lein/lein.symlink/profiles.clj
+++ lein/lein.symlink/profiles.clj
@@ -1,1 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]]}}
+{:user {
+        :plugins [[cider/cider-nrepl "0.8.2"] [jonase/eastwood "0.2.1"]]
+        ; Clojure linting with Eastwood
+        :dependencies [[jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]
+        }}
+
