3978812ed25d79f2560d444162f778a13f24ea1e
Dan Pilone <dan@element84.com>
2015-09-19 14:53:40 -0400

    Adds ctags to brew install.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -14,6 +14,6 @@ then
 fi
 
 # Install homebrew packages
-brew install grc coreutils tree macvim tmux reattach-to-user-namespace python git-extras
+brew install ctags grc coreutils tree macvim tmux reattach-to-user-namespace python git-extras
 
 exit 0
