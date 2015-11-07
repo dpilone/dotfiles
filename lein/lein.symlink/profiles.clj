a9f7bb0598f542a525c6d89332b19ea52b559052
Dan Pilone <dan@element84.com>
2015-11-06 21:07:28 -0500

    Adds ultra repl support.

--- lein/lein.symlink/profiles.clj
+++ lein/lein.symlink/profiles.clj
@@ -1,6 +1,7 @@
 {:user {
-        :plugins [[cider/cider-nrepl "0.8.2"] [jonase/eastwood "0.2.1"]]
+        :plugins [[cider/cider-nrepl "0.8.2"] [jonase/eastwood "0.2.1"][venantius/ultra  "0.3.4"]]
         ; Clojure linting with Eastwood
         :dependencies [[jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]
+        :ultra  {:color-scheme :solarized_dark}
         }}
 
